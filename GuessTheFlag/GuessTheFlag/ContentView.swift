fabad3e1acef4cc8c9ce409d21f782cfb0538870
Robin Malhotra <me@rmalhotra.com>
2020-09-03 00:19:21 +0100

    Add GuessTheFlag
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Robin Malhotra on 02/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		VStack(spacing: 20) {
+			Text("Hello World")
+			Text("This is inside a stack")
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
