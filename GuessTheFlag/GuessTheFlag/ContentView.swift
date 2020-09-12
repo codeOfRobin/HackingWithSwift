fffc6a5b972883f324e0f0566c3dec81b2dea72a
Robin Malhotra <me@rmalhotra.com>
2020-09-13 00:09:48 +0100

    moving in tutorial
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var showingAlert = false
-	var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-	var correctAnswer = Int.random(in: 0...2)
+	@State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+	@State var correctAnswer = Int.random(in: 0...2)
 	@State private var showingScore = false
 	@State private var scoreTitle = ""
 
@@ -20,23 +20,30 @@ struct ContentView: View {
 		} else {
 			scoreTitle = "Wrong"
 		}
-
 		showingScore = true
+	}
+
+	func askQuestion() {
+		countries.shuffle()
+		correctAnswer = Int.random(in: 0...2)
 	}
 	
 	var body: some View {
 		ZStack {
-			Color.blue.edgesIgnoringSafeArea(.all)
+			LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+				.edgesIgnoringSafeArea(.all)
 			VStack(spacing: 30) {
 				VStack {
 					Text("Tap the flag of")
 						.foregroundColor(.white)
 					Text(countries[correctAnswer])
 						.foregroundColor(.white)
+						.font(.largeTitle)
+						.fontWeight(.black)
 				}
 				ForEach(0 ..< 3) { number in
 					Button(action: {
-					   // flag was tapped
+						flagTapped(number)
 					}) {
 						Image(self.countries[number])
 							.renderingMode(.original)
@@ -44,6 +51,10 @@ struct ContentView: View {
 				}
 				Spacer()
 			}
+		}.alert(isPresented: $showingScore) {
+			Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+				self.askQuestion()
+			})
 		}
 	}
 
